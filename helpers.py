30cfefeffef405812279acff9c97210e8f2c5bba
Daniel Namdar <danieln@meetup.com>
2019-03-24 17:36:42 -0400

    first commit

---
+++ helpers.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.metrics import accuracy_score
+from collections import Counter
+
+def cluster_acc(Y,clusterLabels):
+    assert (Y.shape == clusterLabels.shape)
+    pred = np.empty_like(Y)
+    for label in set(clusterLabels):
+        mask = clusterLabels == label
+        sub = Y[mask]
+        target = Counter(sub).most_common(1)[0][0]
+        pred[mask] = target 
+    return accuracy_score(Y,pred)
